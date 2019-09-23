ac6153aa680223beec385c6092c9fb72e8080ef5
dtrts <djt210@gmail.com>
2019-09-23 14:46:20 +0100

    RED-GREEN a single deposit appears on the statement

--- lib/account.rb
+++ lib/account.rb
@@ -1,5 +1,23 @@
 class Account
+  def initialize
+    @transactions = []
+  end
+
+  def deposit(amount)
+    @transactions << [amount, Time.now]
+  end
+
   def statement
-    "date || credit || debit || balance\n"
+    print_statement = "date || credit || debit || balance\n"
+
+    @transactions.each do |transaction|
+      print_statement << transaction[1].strftime('%d/%m/%Y')
+      print_statement << ' || || '
+      print_statement << '%.2f' % transaction[0]
+      print_statement << ' || '
+      print_statement << '%.2f' % transaction[0]
+      print_statement << "\n"
+    end
+    print_statement
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,6 +1,13 @@
 require 'account.rb'
 describe Account do
+  header = "date || credit || debit || balance\n"
   it 'will print the header from the statement' do
-    expect(subject.statement).to eq("date || credit || debit || balance\n")
+    expect(subject.statement).to eq(header)
+  end
+
+  it 'a deposit appears on the statement' do
+    allow(Time).to receive(:now).and_return(Time.new(1990, 2, 15, 0, 0, 0))
+    subject.deposit(100)
+    expect(subject.statement).to eq(header + "15/02/1990 || || 100.00 || 100.00\n")
   end
 end
