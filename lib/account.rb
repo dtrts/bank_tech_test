b2d36b483c60a8fbe61fa4b75bb6115d9e5478d2
dtrts <djt210@gmail.com>
2019-09-23 15:04:28 +0100

    RED-GREEN A single withdrawal is also printed

--- lib/account.rb
+++ lib/account.rb
@@ -7,15 +7,28 @@ class Account
     @transactions << [amount, Time.now]
   end
 
+  def withdraw(amount)
+    @transactions << [-amount, Time.now]
+  end
+
   def statement
     print_statement = "date || credit || debit || balance\n"
 
     @transactions.each do |transaction|
       print_statement << transaction[1].strftime('%d/%m/%Y')
-      print_statement << ' || || '
-      print_statement << '%.2f' % transaction[0]
-      print_statement << ' || '
-      print_statement << '%.2f' % transaction[0]
+      if transaction[0] > 0
+        print_statement << ' || || '
+        print_statement << '%.2f' % transaction[0]
+        print_statement << ' || '
+        print_statement << '%.2f' % transaction[0]
+
+      else
+        print_statement << ' || '
+        print_statement << format('%.2f', (-1 * transaction[0]))
+        print_statement << ' || || '
+        print_statement << '%.2f' % transaction[0]
+    end
+
       print_statement << "\n"
     end
     print_statement
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -10,4 +10,10 @@ describe Account do
     subject.deposit(100)
     expect(subject.statement).to eq(header + "15/02/1990 || || 100.00 || 100.00\n")
   end
+
+  it 'a withdrawal appears on the statement' do
+    allow(Time).to receive(:now).and_return(Time.new(1990, 2, 15, 0, 0, 0))
+    subject.withdraw(100)
+    expect(subject.statement).to eq(header + "15/02/1990 || 100.00 || || -100.00\n")
+  end
 end
