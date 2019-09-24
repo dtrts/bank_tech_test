94ec37cca4f4a840cb147c543c62b36caeeb61a3
dtrts <djt210@gmail.com>
2019-09-24 16:39:44 +0100

    REFACTOR account basic spec to remove dependency on Transaction

--- spec/account/account_basic_spec.rb
+++ spec/account/account_basic_spec.rb
@@ -1,10 +1,14 @@
 require 'account.rb'
 describe Account do
+  test_time = Time.new(1990, 2, 15, 0, 0, 0)
+
   let(:transaction) { double(:transaction) }
-  let(:transaction_class) { double(:transaction_class, new: :transaction) }
+  let(:transaction_class) { double(:transaction_class, new: transaction) }
+  subject { Account.new(transaction_class) }
+
   describe 'basic statements' do
     before(:each) do
-      allow(Time).to receive(:now).and_return(Time.new(1990, 2, 15, 0, 0, 0))
+      allow(Time).to receive(:now).and_return(test_time)
     end
 
     it 'will print the header from the statement' do
@@ -13,6 +17,8 @@ describe Account do
     end
 
     it 'a deposit appears on the statement' do
+      allow(transaction).to receive(:amount).and_return(100)
+      allow(transaction).to receive(:datetime).and_return(test_time)
       subject.deposit(100)
 
       expected_statement = "date || credit || debit || balance\n"
@@ -21,6 +27,8 @@ describe Account do
     end
 
     it 'a withdrawal appears on the statement' do
+      allow(transaction).to receive(:amount).and_return(-100)
+      allow(transaction).to receive(:datetime).and_return(test_time)
       subject.withdraw(100)
 
       expected_statement = "date || credit || debit || balance\n"
