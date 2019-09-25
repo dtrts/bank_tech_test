876b0b6dc7989acf1e09869e932eef5c56891262
dtrts <djt210@gmail.com>
2019-09-25 14:23:40 +0100

    REFACTOR moved unit tests out of individual folders

---
+++ spec/account_spec.rb
@@ -0,0 +1,63 @@
+require 'account'
+
+describe Account do
+  let(:printer) { double(:printer, statement: 'A statement') }
+  let(:printer_class) { double(:printer_class, new: printer) }
+
+  let(:transaction) { double(:transaction) }
+  let(:transaction_class) { double(:transaction_class, new: transaction) }
+
+  subject { Account.new(transaction_class, printer_class) }
+
+  describe '#deposit' do
+    it 'creates a new transaction' do
+      expect(transaction_class).to receive(:new).with(100)
+      subject.deposit(100)
+    end
+    it 'returns the current log' do
+      expect(subject.deposit(100)).to eq([transaction])
+    end
+  end
+
+  describe '#withdraw' do
+    it 'creates a new transaction' do
+      amount = (rand * 1000).round(2)
+      expect(transaction_class).to receive(:new).with(amount)
+      subject.withdraw(-1 * amount)
+    end
+
+    it 'returns the current log' do
+      expect(subject.deposit(100)).to eq([transaction])
+    end
+  end
+
+  context 'after multiple transactions' do
+    it 'returns a transaction log with 3x transactions after 3rd transaction' do
+      subject.deposit(100)
+      subject.withdraw(100)
+      expect(subject.deposit(100)).to eq([transaction,transaction,transaction])
+    end
+  end
+
+
+  describe '#statement' do
+    it 'calls the printer with an array with single transaction - deposit' do
+      expect(printer).to receive(:statement).with([transaction])
+      subject.deposit(100)
+      subject.statement
+    end
+
+    it 'calls the printer with an array with single transaction - withdraw' do
+      expect(printer).to receive(:statement).with([transaction])
+      subject.withdraw(100)
+      subject.statement
+    end
+
+    it 'calls the printer with an array of transactions, x2' do
+      expect(printer).to receive(:statement).with([transaction, transaction])
+      subject.deposit(100)
+      subject.withdraw(100)
+      subject.statement
+    end
+  end
+end
---
+++ spec/transaction_spec.rb
@@ -0,0 +1,62 @@
+require 'transaction.rb'
+
+describe 'Transaction' do
+  describe '#amount' do
+    it 'exposes the value' do
+      100.times do
+        amount = (rand * 1000).round(2)
+        transaction = Transaction.new(amount)
+        expect(transaction.amount).to eq(amount)
+      end
+    end
+    it 'deals with 79.29' do
+      # 79.29 * 100 returns a long float which failed the fractional pence check
+      transaction = Transaction.new(79.29)
+      expect(transaction.amount).to eq(79.29)
+    end
+  end
+
+  describe '#datetime' do
+    it 'exposes the value' do
+      test_time = Time.new(1990, 2, 15, 0, 0, 0)
+      allow(Time).to receive(:now).and_return(test_time)
+      transaction = Transaction.new(100)
+      expect(transaction.datetime).to eq(test_time)
+    end
+  end
+
+  describe '#deposit?' do
+    it 'positive amounts are a deposit' do
+      100.times do
+        amount = (rand * 1000).round(2)
+        transaction = Transaction.new(amount)
+        expect(transaction.deposit?).to eq(true)
+      end
+    end
+  end
+  describe '#withdrawal?' do
+    it 'negative amounts are a deposit' do
+      100.times do
+        amount = -1 * (rand * 1000).round(2)
+        transaction = Transaction.new(amount)
+        expect(transaction.withdrawal?).to eq(true)
+      end
+    end
+  end
+
+  describe 'value checking for deposit and withdrawal' do
+    it 'throws error on fractional pence - deposit' do
+      expect { Transaction.new(0.001) }.to raise_error(Transaction::ERR_FRACTIONAL)
+    end
+    it 'throws error on fractional pence - withdraw' do
+      expect { Transaction.new(-0.001) }.to raise_error(Transaction::ERR_FRACTIONAL)
+    end
+    it 'throws error on string type' do
+      expect { Transaction.new('a string') }.to raise_error(Transaction::ERR_NAN)
+    end
+
+    it 'throws error on string type' do
+      expect { Transaction.new('a string') }.to raise_error(Transaction::ERR_NAN)
+    end
+  end
+end
